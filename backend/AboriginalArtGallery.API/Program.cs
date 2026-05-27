aa2dc069814fa6027fcfe4b7e15caa5d73dc102a
Raghav Thaman <thamanraghav4@gmail.com>
2026-05-27 12:29:44 +0530

    refactor: add library using statements to Program.cs

--- backend/AboriginalArtGallery.API/Program.cs
+++ backend/AboriginalArtGallery.API/Program.cs
@@ -1,4 +1,15 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using AboriginalArtGallery.API.Data;
+using AboriginalArtGallery.API.Middleware;
+using AboriginalArtGallery.API.Repositories.Implementations;
+using AboriginalArtGallery.API.Repositories.Interfaces;
+using AboriginalArtGallery.API.Services.Implementations;
+using AboriginalArtGallery.API.Services.Interfaces;
 
 DotNetEnv.Env.Load();
 
